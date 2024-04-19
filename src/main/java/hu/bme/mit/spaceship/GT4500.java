8bfb153be13150a55cd48197d6bad11a5326fb78
laborgep <you@example.com>
2024-04-19 14:01:34 +0200

    actual implementation for fireTorpedo

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,8 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        firingSuccess = true;
+        firingSuccess = primaryTorpedoStore.fire(1);
+        firingSuccess = secondaryTorpedoStore.fire(1) || firingSuccess;
         break;
     }
 
